6b004d30dc37c81f691932ba2fcf8b7ea88ff1f0
kewerner <werner@biblhertz.it>
2025-05-28 15:03:41 +0200

    adding more points (still lacking geojson polygons)

--- places.js
+++ places.js
@@ -6,6 +6,55 @@ var places = {
         "speed": 1,
         "pitch": 5
     },
+    "VillaTrivulzio": {
+        "center":  [ 12.629723, 41.91662 ],
+        "bearing": 32,
+        "zoom": 16.5,
+        "speed": 1,
+        "pitch": 40
+    },
+    "TerreniAniene": {
+        "center":  [ 12.630838, 41.918616 ],
+        "bearing": 32,
+        "zoom": 16.5,
+        "speed": 1,
+        "pitch": 40
+    },
+    "FontanileBenedettoXIV": {
+        "center":  [ 12.575036, 41.903741 ],
+        "bearing": 32,
+        "zoom": 16.5,
+        "speed": 1,
+        "pitch": 40
+    },
+    "AcquedotttoEmergente": {
+        "center":  [ 12.565932, 41.90207 ],
+        "bearing": 32,
+        "zoom": 16.5,
+        "speed": 1,
+        "pitch": 40
+    },
+    "ViaPietralata": {
+        "center":  [ 12.533035, 41.926767 ],
+        "bearing": 32,
+        "zoom": 16.5,
+        "speed": 1,
+        "pitch": 40
+    },
+    "PiazzaSEmerenziana": {
+        "center":  [ 12.516206, 41.929042 ],
+        "bearing": 32,
+        "zoom": 16.5,
+        "speed": 1,
+        "pitch": 40
+    },
+    "VillaGiulia": {
+        "center":  [ 12.478479, 41.91802 ],
+        "bearing": 32,
+        "zoom": 16.5,
+        "speed": 1,
+        "pitch": 40
+    },
     "Nazzareno": {
         "center":  [ 12.4837635, 41.9026744 ],
         "bearing": 290,
